c90446999645f1662d716062912d3a3afc0b564d
Wong Lok <yellowhappy831@gmail.com>
2017-10-23 13:52:45 +0800

    backup on 2017-10-23 13:52:45

--- functions/seo.js
+++ functions/seo.js
@@ -3,6 +3,7 @@ var functions = require('firebase-functions')
 const express = require('express')
 
 const config = require('./config.js').config
+const convertToFbia = require('article-json-to-fbia')
 
 var fs = require('fs')
 var path = require('path')
@@ -12,31 +13,81 @@ const app = express()
 const cors = require('cors')({origin: true})
 app.use(cors)
 
-var adapter = ({ req, res }) => {
+var composeMetaTags = ({ req, res }) => {
   return new Promise((resolve, reject) => {
     //
-
     resolve(
-      fbia(config.pageID)
+      fbiaMetaTags(config.pageID)
     )
     //
   })
 }
 
-var fbia = (pageID) => {
-  return `<meta property="fb:pages" content="${pageID}" />`
+var composeAppDiv = ({ req, res }) => {
+  return new Promise((resolve, reject) => {
+    var articleJSON = [
+      {
+        type: 'paragraph',
+        children: [
+          {
+            type: 'text',
+            content: 'This is the text and '
+          },
+          {
+            type: 'text',
+            bold: true,
+            content: 'some bold text '
+          },
+          {
+            type: 'text',
+            href: 'http://example.com',
+            content: 'some link'
+          }
+        ]
+      },
+      {
+        type: 'embed',
+        embedType: 'image',
+        src: 'http://example/image.jpg',
+        width: 300,
+        height: 150
+      }
+    ]
+
+    resolve(
+      fbiaContent(
+        convertToFbia(articleJSON)
+      )
+    )
+    //
+  })
 }
 
-var metaEntry = (str, newStr) => {
+var fbiaMetaTags = (pageID) => {
+  return `<meta property="fb:pages" content="${pageID}" />`
+}
+var fbiaContent = (content) => {
+  return content
+}
+
+var metaReplacer = (str, newStr) => {
   return str.replace(`<meta name=loklokfbia content=12345>`, newStr)
+}
+var appDivReplacer = (str, newStr) => {
+  return str.replace(`<div id=app></div>`, newStr)
 }
 
 app.get('*', (req, res) => {
   // res.set('Cache-Control', 'public, max-age=60, s-maxage=180')
-
-  adapter({ req, res })
-    .then((metatags) => {
-      return metaEntry(appHTML, metatags)
+  Promise.all([
+    composeMetaTags({ req, res }),
+    composeAppDiv({ req, res })
+  ])
+    .then((promises) => {
+      var result = appHTML
+      result = metaReplacer(result, promises[0])
+      result = appDivReplacer(result, promises[1])
+      return result
     })
     .then((processedString) => {
       res.send(processedString)
